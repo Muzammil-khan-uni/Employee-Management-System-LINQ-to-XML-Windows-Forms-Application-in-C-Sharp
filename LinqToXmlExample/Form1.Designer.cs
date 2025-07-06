a1efbcdbfb38874f7e7a189d0206cb9ada3cf817
Muzammil Khan <muz56565@gmail.com>
2025-07-06 15:25:50 +0500

    LINQ to XML Employee Management System (Windows Forms Application in C#)

---
+++ LinqToXmlExample/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace LinqToXmlExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up resources being used.
+        /// </summary>
+        /// <param name="disposing">True if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Method for Designer support—do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnLoadXml = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.btnSaveXml = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.txtDesignation = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeight = 29;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 150);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(760, 300);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btnLoadXml
+            // 
+            this.btnLoadXml.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadXml.Name = "btnLoadXml";
+            this.btnLoadXml.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadXml.TabIndex = 1;
+            this.btnLoadXml.Text = "Load XML";
+            this.btnLoadXml.UseVisualStyleBackColor = true;
+            this.btnLoadXml.Click += new System.EventHandler(this.btnLoadXml_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 110);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(93, 110);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(174, 110);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(60, 50);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(189, 20);
+            this.txtID.TabIndex = 6;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(60, 76);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(189, 20);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(316, 50);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(150, 20);
+            this.txtAge.TabIndex = 8;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(12, 53);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(21, 13);
+            this.lblID.TabIndex = 9;
+            this.lblID.Text = "ID:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 79);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 10;
+            this.lblName.Text = "Name:";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(272, 53);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(29, 13);
+            this.lblAge.TabIndex = 11;
+            this.lblAge.Text = "Age:";
+            // 
+            // btnSaveXml
+            // 
+            this.btnSaveXml.Location = new System.Drawing.Point(93, 12);
+            this.btnSaveXml.Name = "btnSaveXml";
+            this.btnSaveXml.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveXml.TabIndex = 2;
+            this.btnSaveXml.Text = "Save XML";
+            this.btnSaveXml.UseVisualStyleBackColor = true;
+            this.btnSaveXml.Click += new System.EventHandler(this.btnSaveXml_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(272, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Salary:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(484, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Designation:";
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(316, 79);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(150, 20);
+            this.txtSalary.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(484, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Company:";
+            // 
+            // txtCompany
+            // 
+            this.txtCompany.Location = new System.Drawing.Point(556, 76);
+            this.txtCompany.Name = "txtCompany";
+            this.txtCompany.Size = new System.Drawing.Size(150, 20);
+            this.txtCompany.TabIndex = 16;
+            // 
+            // txtDesignation
+            // 
+            this.txtDesignation.Location = new System.Drawing.Point(556, 50);
+            this.txtDesignation.Name = "txtDesignation";
+            this.txtDesignation.Size = new System.Drawing.Size(150, 20);
+            this.txtDesignation.TabIndex = 17;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(174, 12);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 18;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.txtDesignation);
+            this.Controls.Add(this.txtCompany);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSaveXml);
+            this.Controls.Add(this.btnLoadXml);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "LINQ to XML Example";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnLoadXml;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Button btnSaveXml;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.TextBox txtDesignation;
+        private System.Windows.Forms.Button Clear;
+    }
+}
---
+++ LinqToXmlExample/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace LinqToXmlExample
+{
+    public partial class Form1 : Form
+    {
+        private XDocument xdoc;
+        private string xmlFilePath = "employees.xml";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadXml_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                xdoc = XDocument.Load(xmlFilePath);
+            }
+            catch
+            {
+                // If file doesn't exist, create a new XDocument
+                xdoc = new XDocument(new XElement("employees"));
+                xdoc.Save(xmlFilePath);
+            }
+
+            BindData();
+        }
+
+        private void BindData()
+        {
+            var query = from employee in xdoc.Descendants("Employee")
+                        select new
+                        {
+                            ID = (int)employee.Element("ID"),
+                            Name = (string)employee.Element("Name"),
+                            Age = (int)employee.Element("Age"),
+                            Salary= (string)employee.Element("Salary"),
+                            Designation= (string)employee.Element("Designation"),
+                            Company = (string)employee.Element("Company")
+                        };
+
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                XElement newEmployee = new XElement("Employee",
+                    new XElement("ID", int.Parse(txtID.Text)),
+                    new XElement("Name", txtName.Text),
+                    new XElement("Age", int.Parse(txtAge.Text)),
+                    new XElement("Salary", txtSalary.Text),
+                    new XElement("Designation", txtDesignation.Text),
+                    new XElement("Company", txtCompany.Text)
+                );
+
+                xdoc.Root.Add(newEmployee);
+                xdoc.Save(xmlFilePath);
+                BindData();
+                MessageBox.Show("Employee data Added Successfully.");
+                ClearInputFields();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                XElement employee = xdoc.Descendants("Employee")
+                    .Where(s => (int)s.Element("ID") == int.Parse(txtID.Text))
+                    .FirstOrDefault();
+
+                if (employee != null)
+                {
+                    employee.SetElementValue("Name", txtName.Text);
+                    employee.SetElementValue("Age", int.Parse(txtAge.Text));
+                    employee.SetElementValue("Salary", txtSalary.Text);
+                    employee.SetElementValue("Designation", txtDesignation.Text);
+                    employee.SetElementValue("Company", txtCompany.Text);
+
+                    xdoc.Save(xmlFilePath);
+                    BindData();
+                    MessageBox.Show("Employee data Updated Successfully.");
+                    ClearInputFields();
+                }
+                else
+                {
+                    MessageBox.Show("Employee not found for updating.");
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id;
+            if (int.TryParse(txtID.Text, out id))
+            {
+                XElement employee = xdoc.Descendants("Employee")
+                    .Where(s => (int)s.Element("ID") == id)
+                    .FirstOrDefault();
+
+                if (employee != null)
+                {
+                    employee.Remove();
+                    xdoc.Save(xmlFilePath);
+                    BindData();
+                    MessageBox.Show("Employee data Deleted Successfully.");
+                    ClearInputFields();
+                }
+                else
+                {
+                    MessageBox.Show("Employee not found for deletion.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid ID to delete.");
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            int id, age;
+            if (!int.TryParse(txtID.Text, out id))
+            {
+                MessageBox.Show("Please enter a valid ID.");
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("Please enter a Name.");
+                return false;
+            }
+            if (!int.TryParse(txtAge.Text, out age))
+            {
+                MessageBox.Show("Please enter a valid Age.");
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtSalary.Text))
+            {
+                MessageBox.Show("Please enter a Salary.");
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtDesignation.Text))
+            {
+                MessageBox.Show("Please enter a Designation.");
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtCompany.Text))
+            {
+                MessageBox.Show("Please enter a Company.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ClearInputFields()
+        {
+            txtID.Text = "";
+            txtName.Text = "";
+            txtAge.Text = "";
+            txtSalary.Text = "";
+            txtDesignation.Text = "";
+            txtCompany.Text = "";
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Populate input fields when a row is selected
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtID.Text = row.Cells["ID"].Value.ToString();
+                txtName.Text = row.Cells["Name"].Value.ToString();
+                txtAge.Text = row.Cells["Age"].Value.ToString();
+                txtSalary.Text = row.Cells["Salary"].Value.ToString();
+                txtDesignation.Text = row.Cells["Designation"].Value.ToString();
+                txtCompany.Text = row.Cells["Company"].Value.ToString();
+            }
+        }
+
+        private void btnSaveXml_Click(object sender, EventArgs e)
+        {
+            xdoc.Save(xmlFilePath);
+            MessageBox.Show("XML file saved successfully.");
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            txtID.Text = "";
+            txtName.Text = "";
+            txtAge.Text = "";
+            txtSalary.Text = "";
+            txtDesignation.Text = "";
+            txtCompany.Text = "";
+        }
+    }
+}
